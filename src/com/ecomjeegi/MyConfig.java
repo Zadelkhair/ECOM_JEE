80f61903c8f8ced648c1075ef93bab16ff9d8f94
ayachhamza <hamza.ayach.98@gmail.com>
2021-06-15 14:54:29 +0100

    pushing the servlets and added mapping url in xml file

--- src/com/ecomjeegi/MyConfig.java
+++ src/com/ecomjeegi/MyConfig.java
@@ -6,4 +6,16 @@ public abstract class MyConfig {
 		return "http://localhost:8081/ecom_jee_gi/";
 	}
 	
+	public static String getDbName() {
+		return "e_comerce_db"; 
+	}
+	public static String getDbUrl() {
+			return "jdbc:mysql://127.0.0.1";
+		}
+	public static String getDbUserName() {
+		return "root";
+	}
+	public static String getDbPassword() {
+		return "";
+	}
 }
