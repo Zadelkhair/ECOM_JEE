c3af5857ff820cfd8802f1162481daab18dca432
ayachhamza <hamza.ayach.98@gmail.com>
2021-06-16 11:17:04 +0100

    adding servlets and jsp views ,also database configuration and mapping of the servlets in the web xml file

--- src/com/ecomjeegi/MyConfig.java
+++ src/com/ecomjeegi/MyConfig.java
@@ -5,5 +5,18 @@ public class MyConfig {
 	public static String getHost() {
 		return "http://localhost:8081/ecom_jee_gi/";
 	}
+	public static String getDbUrl(){
+		return "jdbc:mysql://127.0.0.1";
+	}
+	public static String getDbName(){
+		return "e_comerce_db";
+	}
+	public static String getDbUserName(){
+		return "root";
+	}
+	public static String getDbPassword(){
+		return "";
+	}
+
 	
 }
